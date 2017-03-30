4d1d244c620ea4f4afd326bb6fd2255c5296e9a0
Akshat Sharma <akshatsharma46@gmail.com>
2017-03-29 18:28:47 -0700

    view-all-boards button; public/private boards seperation

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -2,17 +2,54 @@
  * Created by Akshat on 3/18/2017.
  */
 $("#board-listener").click(function () {
-    $("#show-boards").addClass("visible");
-    $("#close-show").addClass("visible");
-    $(".title-secondary").addClass("visible");
-    $(".boards-wrapper").addClass("visible");
-    $("#underline").addClass("expand");
+    $("#all").addClass("selected");
+    $("#all-boards").show();
+    $("#public-boards").hide();
+    $("#private-boards").hide();
+
+    $("#show-boards").animate({bottom: "-20px"});
+    $("#close-show").stop().delay(800).hide().fadeIn(300);
+    $(".title-secondary").stop().delay(800).hide().fadeIn(800);
+    $(".filter-buttons").stop().delay(1000).hide().fadeIn(800);
+    $(".boards-wrapper").stop().delay(1300).hide().fadeIn(1000);
+    $("#underline").hide().fadeIn(500).addClass("expand");
 });
 
 $("#close-show").click(function () {
-    $("#show-boards").removeClass("visible");
-    $("#close-show").removeClass("visible");
-    $(".title-secondary").removeClass("visible");
-    $(".boards-wrapper").removeClass("visible");
-    $("#underline").removeClass("expand");
+    $("#show-boards").animate({bottom: "100%"});
+    $("#close-show").fadeOut();
+    $(".title-secondary").fadeOut();
+    $(".filter-buttons").fadeOut();
+    $(".boards-wrapper").fadeOut();
+    $("#underline").fadeOut();
+    $("#all").removeClass("selected");
+    $("#public").removeClass("selected");
+    $("#private").removeClass("selected");
+});
+
+$("#all").click(function () {
+    $("#public").removeClass("selected");
+    $("#private").removeClass("selected");
+    $(this).toggleClass("selected");
+    $("#all-boards").fadeIn();
+    $("#public-boards").hide();
+    $("#private-boards").hide();
+});
+
+$("#public").click(function () {
+    $("#all").removeClass("selected");
+    $("#private").removeClass("selected");
+    $(this).toggleClass("selected");
+    $("#all-boards").hide();
+    $("#public-boards").fadeIn();
+    $("#private-boards").hide();
+});
+
+$("#private").click(function () {
+    $("#all").removeClass("selected");
+    $("#public").removeClass("selected");
+    $(this).toggleClass("selected");
+    $("#all-boards").hide();
+    $("#public-boards").hide();
+    $("#private-boards").fadeIn();
 });

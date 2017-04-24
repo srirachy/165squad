0e388e6951a24b14e9a80633c0faa9198b6226f0
Adrian <amorhuerta@yahoo.com>
2017-04-23 23:50:22 -0700

    added feed data from database

---
+++ public/javascripts/Controllers/ctrlFeed.js
@@ -0,0 +1,13 @@
+// angular.module('myApp').controller('ctrlFeed', ['$scope', '$uibModal', '$log', '$http', function($scope, $uibModal, $log, $http){
+
+// 	$http.get('/api/feed')
+//     .success(function (result){
+//       $scope.pins = result;
+//     })
+//     .error(function (data, status){
+//       console.log(data);
+//     });
+
+
+
+// }]); 
